1a18a3b2513d07658c15b8a30a46daf8486920f1
Dhruv Mohindru <dmohindru@gmail.com>
2023-07-24 07:20:26 +1000

    Finished section 23

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,6 +10,8 @@ const isLocalApiError = (err: any): err is LocalApiError => {
     return typeof err.code === "string";
 };
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
@@ -18,7 +20,7 @@ export const serveCommand = new Command()
         try {
             
             const dir = path.join(process.cwd(), path.dirname(filename));
-            await serve(parseInt(options.port), path.basename(filename), dir);
+            await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
             console.log(
                 `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
             )
